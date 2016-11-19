92da1058eb310b2a674f0cc2faa9edf3bf30cbc9
Cameron Fabbri <cameronfabbri@gmail.com>
2016-11-19 15:23:29 -0500

    added comments

--- examples/autoencoder.py
+++ examples/autoencoder.py
@@ -1,37 +1,40 @@
+import matplotlib.pyplot as plt
+import cPickle as pickle
 import tensorflow as tf
 import numpy as np
-import os
-import sys
-import numpy as np
-import cv2
-import time
 import requests
-from tqdm import tqdm
 import gzip
-import cPickle as pickle
+import os
+
 from tensorflow_ops import _conv_layer, _transpose_conv_layer, _fc_layer, lrelu
-import matplotlib.pyplot as plt
-import random
 
 batch_size = 10
 
 def encoder(x):
+   # convolutional layer with a leaky Relu activation
    e_conv1 = lrelu(_conv_layer(x, 2, 2, 32, 'e_conv1'))
    print 'conv1: ', e_conv1
    print
 
+   # convolutional layer with a leaky Relu activation
    e_conv2 = lrelu(_conv_layer(e_conv1, 2, 2, 64, 'e_conv2'))
    print 'conv2: ', e_conv2
    print
    
+   # convolutional layer with a leaky Relu activation
    e_conv3 = lrelu(_conv_layer(e_conv2, 2, 2, 32, 'e_conv3'))
    print 'conv3: ', e_conv3
    print
-   
+  
+   # fully connected layer with a leaky Relu activation
+   # The 'True' here means that we are flattening the input
    e_fc1 = lrelu(_fc_layer(e_conv2, 512, True, 'e_fc1'))
    print 'fc1: ', e_fc1
    print
 
+   # fully connected layer with a leaky Relu activation
+   # the output from the previous fully connected layer is
+   # already flat, so no need to flatten, hence 'False'
    e_fc2 = lrelu(_fc_layer(e_fc1, 256, False, 'e_fc2'))
    print 'fc2: ', e_fc2
    print
@@ -41,25 +44,34 @@ def encoder(x):
 def decoder(x):
    print 'x: ', x
    print
+  
    
+   # fully connected layer with a leaky Relu activation
    d_fc1 = lrelu(_fc_layer(x, 512, False, 'd_fc1'))
+
+   # reshape for use in transpose convolution (deconvolution) 
+   # must match conv layers in encoder
    d_fc1 = tf.reshape(d_fc1, (batch_size, 4, 4, 32))
    print 'd_fc1: ', d_fc1
    print
-  
+ 
+   # transpose convolution with a leaky relu activation
    e_transpose_conv1 = lrelu(_transpose_conv_layer(d_fc1, 2, 2, 32, 'e_transpose_conv1'))
    print 'e_transpose_conv1: ', e_transpose_conv1
    print
 
+   # transpose convolution with a leaky relu activation
    e_transpose_conv2 = lrelu(_transpose_conv_layer(e_transpose_conv1, 2, 2, 64, 'e_transpose_conv2'))
    print 'e_transpose_conv2: ', e_transpose_conv2
    print
    
+   # transpose convolution with a leaky relu activation
    e_transpose_conv3 = lrelu(_transpose_conv_layer(e_transpose_conv2, 2, 2, 1, 'e_transpose_conv3'))
    print 'e_transpose_conv3: ', e_transpose_conv3
    print
 
-   #e_transpose_conv3 = tf.reshape(e_transpose_conv3, (batch_size, 28, 28, 1))
+   # since transpose convs make the resolution go 4->8->16->32 (because stride 2)
+   # we need to crop to original mnist size (28,28)
    e_transpose_conv3 = e_transpose_conv3[:,:28,:28,:]
    return e_transpose_conv3
 
@@ -67,13 +79,18 @@ def decoder(x):
 def train(mnist_train, mnist_test):
    with tf.Graph().as_default():
       global_step = tf.Variable(0, trainable=False, name='global_step')
-      epoch_num   = tf.Variable(0, trainable=False, name='epoch_num')
 
+      # placeholder for mnist images
       images      = tf.placeholder(tf.float32, [batch_size, 28, 28, 1])
 
+      # encode images to 256 dim vector
       encoded = encoder(images)
+
+      # decode 256 dim vector to (28,28) dim image
       decoded = decoder(encoded)
 
+      # loss is the squared error between the true image and the one we
+      # decoded from the 256 dim vector
       loss = tf.reduce_mean(tf.pow(images - decoded, 2))
      
       train_op = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
@@ -82,26 +99,36 @@ def train(mnist_train, mnist_test):
       sess = tf.Session()
       sess.run(init)
 
-      # saver for the model
-      saver = tf.train.Saver(tf.all_variables())
-      
       step = 0
       while True:
          step += 1
+
+         # get random images from the training set
          batch_images = random.sample(mnist_train, batch_size)
+
+         # send through the network
          _, loss_ = sess.run([train_op, loss], feed_dict={images: batch_images})
          print 'Step: ' + str(step) + ' Loss: ' + str(loss_)
 
          if step%1000 == 0:
+
+            # get random images from the test set
             batch_images = random.sample(mnist_test, batch_size)
+
+            # encode them using the encoder, then decode them
             encode_decode = sess.run(decoded, feed_dict={images: batch_images})
+
+            # write out to files
             for img in encode_decode:
                img = np.squeeze(img)
                plt.imsave('images/image_'+str(step)+'.png', img)
 
+
 def main(argv=None):
+   # mnist data in gz format
    url = 'http://deeplearning.net/data/mnist/mnist.pkl.gz'
 
+   # check if it's already downloaded
    if not os.path.isfile('./mnist.pkl.gz'):
       print 'Downloading mnist...'
       with open('mnist.pkl.gz', 'wb') as f:
@@ -110,6 +137,7 @@ def main(argv=None):
             f.write(r.content)
          else:
             print 'Could not connect to ', url
+
    f = gzip.open('mnist.pkl.gz', 'rb')
    train_set, val_set, test_set = pickle.load(f)
 
@@ -131,4 +159,3 @@ def main(argv=None):
 
 if __name__ == '__main__':
    tf.app.run()
-
